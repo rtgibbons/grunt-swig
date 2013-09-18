cef50e4fe9113b8b819851f890515db04e3d03ce
Paul-Emile MINY <paulemile.miny@gmail.com>
2013-09-18 08:14:47 +0200

    restore old test features and create new test fixtures to test tplFile variable feature

--- test/swig_test.js
+++ test/swig_test.js
@@ -21,11 +21,19 @@ helpers.assertFile = function (file, reg) {
 describe('grunt-swig', function() {
 
   it('should create dest/index html', function(){
-    helpers.assertFile('test/dest/index.html', /^\[index, index\] Hello short path file, Hello world$/);
+    helpers.assertFile('test/dest/index.html', /^Hello short path file, Hello world$/);
   });
 
   it('should create dest/dest/path/to/index.html', function(){
-    helpers.assertFile('test/dest/path/to/index.html', /^\[path\/to\/index, index\] Hello long path file$/);
+    helpers.assertFile('test/dest/path/to/index.html', /^\Hello long path file$/);
+  });
+
+  it('should create dest/tplFile html', function(){
+    helpers.assertFile('test/dest/tplFile.html', /^tplFile, tplFile$/);
+  });
+
+  it('should create dest/path/to/tplFile html', function(){
+    helpers.assertFile('test/dest/path/to/tplFile.html', /^path\/to\/tplFile, tplFile$/);
   });
 
   it('should create dest/sitemap.xml', function(){
