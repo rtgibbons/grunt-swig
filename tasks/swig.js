9802398c2f07f395155278e1da183e29ae47cf90
Nick Pack <nick@nickpack.com>
2013-09-23 20:54:38 +0100

    Initial hacktogether of Swig 1.0.0 support

--- tasks/swig.js
+++ tasks/swig.js
@@ -6,23 +6,22 @@ module.exports = function(grunt) {
       swig = require('swig'),
       path = require('path');
 
-  grunt.registerMultiTask('swig', 'swig templater', function(context) {
+  grunt.registerMultiTask('swig', 'swig templater', function(tpl_context) {
     var config = this,
-        context = context || '',
+        context = tpl_context || '',
         file_re = /(.*)\.([A-Za-z]+)/i,
         pages = [],
-        d = new Date;
-        d = d.toISOString()
+        date = new Date,
+        d = date.toISOString();
         
     var defaultPriority = config.data.sitemap_priorities !== undefined && config.data.sitemap_priorities['_DEFAULT_'] !== undefined ? config.data.sitemap_priorities['_DEFAULT_'] : '0.5',
         generateSitemap = config.data.generateSitemap !== undefined ? config.data.generateSitemap : true,
         generateRobotstxt = config.data.generateRobotstxt !== undefined ? config.data.generateRobotstxt : true;
     
-    swig.init(config.data.init);
+    swig.Swig(config.data.init);
 
     config.filesSrc.forEach(function(filename) {
       var file = file_re.exec(filename)[1],
-          tpl = swig.compileFile(filename),
           htmlFile = config.data.dest + file + ".html",
           tplVars = {},
           contextVars = {},
@@ -57,7 +56,7 @@ module.exports = function(grunt) {
 
       grunt.log.writeln('Writing HTML to ' + htmlFile);
 
-      grunt.file.write(htmlFile, tpl.render(grunt.util._.extend(globalVars, tplVars, contextVars)));
+      grunt.file.write(htmlFile, swig.renderFile(config.data.init.root + filename, grunt.util._.extend(globalVars, tplVars, contextVars)));
 
       if (config.data.sitemap_priorities !== undefined && config.data.sitemap_priorities[file] !== undefined) {
         pages.push({
@@ -79,16 +78,13 @@ module.exports = function(grunt) {
 
     if (generateSitemap) {
       grunt.log.writeln('Creating sitemap.xml');
-      swig.init( { root: __dirname + '/../'});
-      var sitemaptpl = swig.compileFile( 'templates/sitemap.xml.swig');
-      grunt.file.write(config.data.dest + 'sitemap.xml', sitemaptpl.render({ pages: pages}));
+      grunt.file.write(config.data.dest + 'sitemap.xml', swig.renderFile(__dirname + '/../templates/sitemap.xml.swig', { pages: pages}));
     }
 
     if (generateRobotstxt) {
       grunt.log.writeln('Creating robots.txt');
-      var robotstpl = swig.compileFile( 'templates/robots.txt.swig');
-      grunt.file.write(config.data.dest + 'robots.txt', robotstpl.render({ robots_directive: config.data.robots_directive }));
+      grunt.file.write(config.data.dest + 'robots.txt', swig.renderFile(__dirname + '/../templates/robots.txt.swig', { robots_directive: config.data.robots_directive }));
     }
 
   });
-}
+};
