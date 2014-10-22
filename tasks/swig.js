c846f5613246e0a83f4487bab92f86c4f924e0fc
Ken Goldfarb <hello@kengoldfarb.com>
2014-10-22 16:43:15 -0600

    cleanup and beautify

--- tasks/swig.js
+++ tasks/swig.js
@@ -1,19 +1,19 @@
 'use strict';
-var fs = require('fs'),
-    swig = require('swig'),
-    path = require('path');
+var fs = require('fs');
+var swig = require('swig');
+var path = require('path');
 
 module.exports = function(grunt) {
     grunt.registerMultiTask('swig', 'swig templater', function(tpl_context) {
         var config = this,
-            context = tpl_context || '',
-            date = new Date(),
-            d = date.toISOString(),
-            globalVars = {};
+            var context = tpl_context || '';
+        var date = new Date();
+        var d = date.toISOString();
+        var globalVars = {};
 
         var options = config.options();
 
-        if(options.swigOptions) {
+        if (options.swigOptions) {
             swig.setDefaults(options.swigOptions);
         }
 
@@ -22,23 +22,23 @@ module.exports = function(grunt) {
         var baseUrl = options.siteUrl || '';
 
         config.filesSrc.forEach(function(file) {
-            if(!file) {
+            if (!file) {
                 grunt.log.warn('Source file not found.');
-            }else{
+            } else {
 
                 var tpl = swig.compileFile(file);
                 var filePathArr = file.split('/');
-                var fullFileName = filePathArr[filePathArr.length -1];
+                var fullFileName = filePathArr[filePathArr.length - 1];
                 var matches = fullFileName.match(/(.*)\.([^\.]*)\.?([^\.]*)$/);
 
-                if(matches && matches[1] && matches[2]) {
+                if (matches && matches[1] && matches[2]) {
                     var fileName = matches[1];
                     var ext = matches[2];
                     var templateExtension = matches[3];
-                    if(!templateExtension) {
+                    if (!templateExtension) {
                         templateExtension = ext;
                         ext = '';
-                    }else{
+                    } else {
                         ext = '.' + ext
                     }
 
@@ -46,16 +46,16 @@ module.exports = function(grunt) {
                     var outputFile = config.data.dest + relativeOutputFile;
 
                     // Only process files that end in .swig
-                    if(templateExtension == 'swig') {
+                    if (templateExtension == 'swig') {
                         // Swig file match.  Build up all the data required to create the destination file.
                         pages[outputFile] = pages[outputFile] || {};
                         pages[outputFile].src = fullFileName;
                         pages[outputFile].dest = outputFile;
 
                         var templateData = {};
-                        try{
+                        try {
                             templateData = grunt.file.readJSON(path.dirname(file) + '/' + fileName + ext + '.json');
-                        }catch(e){
+                        } catch (e) {
                             grunt.log.warn('No json file corresponding with: ' + file + '.  Using only global template data.');
                         }
                         pages[outputFile].templateData = grunt.util._.extend(globalTemplateData, templateData);
@@ -69,24 +69,24 @@ module.exports = function(grunt) {
                         pages[outputFile].priority = '0.8';
 
                         // Check if this file has specific sitemap properties set
-                        if(options.sitemap && options.sitemap[relativeOutputFile]) {
+                        if (options.sitemap && options.sitemap[relativeOutputFile]) {
                             pages[outputFile].changefreq = options.sitemap[relativeOutputFile].changefreq || pages[outputFile].changefreq;
                             pages[outputFile].lastmod = options.sitemap[relativeOutputFile].lastmod || pages[outputFile].lastmod;
                             pages[outputFile].priority = options.sitemap[relativeOutputFile].priority || pages[outputFile].priority;
-                        }else if(options.sitemap && options.sitemap.default) {
+                        } else if (options.sitemap && options.sitemap.default) {
                             // If not, check to see if there are defaults
                             pages[outputFile].changefreq = options.sitemap.default.changefreq || pages[outputFile].changefreq;
                             pages[outputFile].lastmod = options.sitemap.default.lastmod || pages[outputFile].lastmod;
                             pages[outputFile].priority = options.sitemap.default.priority || pages[outputFile].priority;
                         }
                     }
-                }else{
+                } else {
                     grunt.log.warn('Unable to process file as template: ' + fullFileName);
                 }
             }
         });
 
-        for(var i in pages) {
+        for (var i in pages) {
             var page = pages[i];
             grunt.log.writeln('Creating file: ' + page.dest);
             grunt.file.write(page.dest, page.tpl(page.templateData));
